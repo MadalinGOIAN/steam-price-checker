bea7e11281e6eb5afc03ef303eb2b10995c6ebeb
Mădălin-Florin GOIAN <79133273+MadalinGOIAN@users.noreply.github.com>
2025-12-24 18:58:23 +0200

    Implement application loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,51 @@
+#include <iostream>
+#include <conio.h>
+
+#include "HttpClient.hpp"
+#include "SteamApiFetcher.hpp"
+#include "SteamGameRepository.hpp"
+#include "Paginator.hpp"
+
+#include "utils/Print.hpp"
+
 int main()
 {
-	return 0;
+    try
+    {
+        HttpClient httpClient;
+        SteamApiFetcher apiFetcher(httpClient);
+        SteamGameRepository repository(apiFetcher);
+
+        std::vector<Game> games = repository.getDiscountedGames();
+
+        if (games.empty())
+        {
+            std::cout << "No discounted games found.\n";
+            return 0;
+        }
+
+        Paginator<Game> paginator(std::move(games), 4u);
+
+        while (paginator.hasNext())
+        {
+            std::vector<Game> page = paginator.next();
+            printGames(page);
+
+            if (!paginator.hasNext())
+            {
+                std::cout << "\nEnd of results.\n";
+                break;
+            }
+
+            std::cout << "\nPress any key to see more...\n";
+            _getch();
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Fatal error: " << e.what() << '\n';
+        return 1;
+    }
+
+    return 0;
 }
